d52d628b4dcdafef040cbc0428fd867e3d657869
araxnoid-code <araxnoid0@gmail.com>
2026-05-24 04:14:02 +0700

    Removed PrevExecutable from the ExecutableTask enum
    Added ExecutableTask::Dummy and the ExecutableTask::new_dummy method to create a dummy
    
    Made adjustments due to the addition of one state to ExecutableTask
    
    Developed SecondaryList, which now has three properties:
    1. dummy, a dummy task that ensures the list is not empty
    2. head, where tasks will be entered by the producer
    3. tail, where tasks will be exited by the consumer
    
    Developed SecondaryList, which now has the following methods:
    1. SecondaryList::new
    2. SecondaryList::push_front
    3. SecondaryList::pop_back

--- src/orqestra/ring_buffer_core/exec/executable_task.rs
+++ src/orqestra/ring_buffer_core/exec/executable_task.rs
@@ -53,16 +53,9 @@ where
     O: 'static,
 {
     /// save the spawned Task
-    Task(
-        WaitingTask<T, O>,
-        NextExecutable<T, J, O>,
-        PrevExecutable<T, J, O>,
-    ),
-    Job(
-        Arc<InnerJob<J, O>>,
-        NextExecutable<T, J, O>,
-        PrevExecutable<T, J, O>,
-    ),
+    Task(WaitingTask<T, O>, NextExecutable<T, J, O>),
+    Job(Arc<InnerJob<J, O>>, NextExecutable<T, J, O>),
+    Dummy(NextExecutable<T, J, O>),
 }
 
 impl<J, T, O> ExecutableTask<T, J, O>
@@ -72,32 +65,29 @@ where
     O: 'static,
 {
     pub fn new_task(task: T) -> ExecutableTask<T, J, O> {
-        Self::Task(
-            WaitingTask::new(task),
-            NextExecutable::new(),
-            PrevExecutable::new(),
-        )
+        Self::Task(WaitingTask::new(task), NextExecutable::new())
     }
 
     pub fn new_job(job: Job<J, O>) -> ExecutableTask<T, J, O> {
-        Self::Job(
-            job.inner.clone(),
-            NextExecutable::new(),
-            PrevExecutable::new(),
-        )
+        Self::Job(job.inner.clone(), NextExecutable::new())
     }
 
     pub fn new_job_from_arc_inner(inner: Arc<InnerJob<J, O>>) -> ExecutableTask<T, J, O> {
-        Self::Job(inner, NextExecutable::new(), PrevExecutable::new())
+        Self::Job(inner, NextExecutable::new())
+    }
+
+    pub fn new_dummy() -> ExecutableTask<T, J, O> {
+        Self::Dummy(NextExecutable::new())
     }
 
     /// execute ExecutableTask
     pub(crate) fn execute(&self) -> O {
         match self {
-            ExecutableTask::Task(task, _, _) => task.f.execute(),
-            ExecutableTask::Job(job, _, _) => job.f.execute(JobDep {
+            ExecutableTask::Task(task, _) => task.f.execute(),
+            ExecutableTask::Job(job, _) => job.f.execute(JobDep {
                 vec: job.dep.take(),
             }),
+            ExecutableTask::Dummy(_) => panic!("Error, dummy cannot be executed"),
         }
     }
 
@@ -106,7 +96,7 @@ where
     /// 1. exec_counter > 0, will not be included in the ring-buffer.
     /// 2. exec_counter == 0, will be put into the ring-buffer.
     pub fn next_job(&self, saving_jobs: &mut VecDeque<ExecutableTask<T, J, O>>) {
-        if let ExecutableTask::Job(job, _, _) = self {
+        if let ExecutableTask::Job(job, _) = self {
             for job in job.next_jobs.take() {
                 if job.exec_counter.fetch_sub(1, Ordering::Relaxed) == 1 {
                     saving_jobs.push_back(Self::new_job_from_arc_inner(job));
@@ -118,14 +108,15 @@ where
     /// update return_value based on parameter value
     pub(crate) fn update_value(&self, value: O) {
         match self {
-            ExecutableTask::Task(task, _, _) => {
+            ExecutableTask::Task(task, _) => {
                 task.return_value.0.replace(Some(value));
                 task.return_value.1.store(true, Ordering::Relaxed);
             }
-            ExecutableTask::Job(job, _, _) => {
+            ExecutableTask::Job(job, _) => {
                 job.return_value.0.replace(Some(value));
                 job.return_value.1.store(true, Ordering::Relaxed);
             }
+            ExecutableTask::Dummy(_) => (),
         };
     }
 
@@ -137,16 +128,9 @@ where
     ///
     pub(crate) fn next(&self) -> &AtomicPtr<ExecutableTask<T, J, O>> {
         match self {
-            ExecutableTask::Task(_, next, _) => &next.0,
-            ExecutableTask::Job(_, next, _) => &next.0,
-        }
-    }
-
-    ///
-    pub(crate) fn prev(&self) -> &AtomicPtr<ExecutableTask<T, J, O>> {
-        match self {
-            ExecutableTask::Task(_, _, prev) => &prev.0,
-            ExecutableTask::Job(_, _, prev) => &prev.0,
+            ExecutableTask::Task(_, next) => &next.0,
+            ExecutableTask::Job(_, next) => &next.0,
+            ExecutableTask::Dummy(next) => &next.0,
         }
     }
 }
--- src/orqestra/ring_buffer_core/secondary_list/core.rs
+++ src/orqestra/ring_buffer_core/secondary_list/core.rs
@@ -1,4 +1,7 @@
-use std::sync::atomic::AtomicPtr;
+use std::{
+    ptr::null_mut,
+    sync::atomic::{AtomicPtr, Ordering},
+};
 
 use crate::{ExecutableTask, OrqestraJobTrait, OrqestraTaskTrait};
 
@@ -8,6 +11,54 @@ where
     J: OrqestraJobTrait<O> + 'static,
     O: 'static,
 {
+    dummy: AtomicPtr<ExecutableTask<T, J, O>>,
     head: AtomicPtr<ExecutableTask<T, J, O>>,
     tail: AtomicPtr<ExecutableTask<T, J, O>>,
+}
+
+impl<T, J, O> SecondaryList<T, J, O>
+where
+    T: OrqestraTaskTrait<O> + 'static,
+    J: OrqestraJobTrait<O> + 'static,
+    O: 'static,
+{
+    pub(crate) fn new() -> SecondaryList<T, J, O> {
+        let dummy_ptr = Box::into_raw(Box::new(ExecutableTask::new_dummy()));
+        Self {
+            dummy: AtomicPtr::new(dummy_ptr),
+            head: AtomicPtr::new(dummy_ptr),
+            tail: AtomicPtr::new(dummy_ptr),
+        }
+    }
+
+    pub(crate) fn push_front(&self, task: ExecutableTask<T, J, O>) {
+        let new_head = Box::into_raw(Box::new(task));
+        let prev_head = self.head.swap(new_head, Ordering::Relaxed);
+
+        unsafe { (*prev_head).next().store(new_head, Ordering::Relaxed) };
+    }
+
+    pub(crate) fn pop_back(&self) -> Option<Box<ExecutableTask<T, J, O>>> {
+        unsafe {
+            let dummy = self.tail.load(Ordering::Relaxed);
+            let executable_task_ptr = (*dummy).next().load(Ordering::Relaxed);
+            if executable_task_ptr.is_null() {
+                return None;
+            }
+
+            let candidate_task_ptr = (*executable_task_ptr).next().load(Ordering::Relaxed);
+
+            let executable_ptr = match (*dummy).next().compare_exchange(
+                executable_task_ptr,
+                candidate_task_ptr,
+                Ordering::Release,
+                Ordering::Acquire,
+            ) {
+                Err(_) => return None,
+                Ok(executable) => executable,
+            };
+
+            Some(Box::from_raw(executable_ptr))
+        }
+    }
 }
